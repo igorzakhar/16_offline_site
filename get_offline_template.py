e983284bf5c9ab5de59793ca041d83cacccfe0a8
igorzakhar <igorzakhar@gmail.com>
2018-01-25 12:22:36 +0300

    Decomposition get_links_from_html() and retrieve_files() functions

--- get_offline_template.py
+++ get_offline_template.py
@@ -11,34 +11,33 @@ def fetch_html(url):
     return webpage
 
 
-def get_links_from_html(html, url):
-    links = []
-    soup = BeautifulSoup(html, 'lxml')
-
+def retrieve_js_files(soup, url):
     js_links = soup.find_all('script', {'src': True})
     for js_link in js_links:
-        absolute_link_js = urllib.parse.urljoin(url, js_link.get('src'))
-        links.append(absolute_link_js)
+        abs_link_js = urllib.parse.urljoin(url, js_link.get('src'))
+        filename = abs_link_js.split('/')[-1]
+        save_path = 'js/' + filename
+        js_link.attrs['src'] = save_path
+        urllib.request.urlretrieve(abs_link_js, save_path)
 
-    css_links = soup.find_all('link', {'href': True})
+
+def retrieve_css_files(soup, url):
+    css_links = soup.find_all('link', {'rel': 'stylesheet'})
     for css_link in css_links:
-        absolute_link_css = urllib.parse.urljoin(url, css_link.get('href'))
-        links.append(absolute_link_css)
-    return links
+        abs_link_css = urllib.parse.urljoin(url, css_link.get('href'))
+        filename = abs_link_css.split('/')[-1]
+        save_path = 'css/' + filename
+        css_link.attrs['href'] = save_path
+        urllib.request.urlretrieve(abs_link_css, save_path)
 
 
-def retrieve_files(links):
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-    urllib.request.install_opener(opener)
-    for link in links:
-        filename = link.split('/')[-1]
-        ext = filename.split('.')[-1]
-        save_path = '{}/{}'.format(ext, filename)
-        if (ext != 'js') and (ext != 'css'):
-            urllib.request.urlretrieve(link, filename)
-        else:
-            urllib.request.urlretrieve(link, save_path)
+def retrieve_favicion(soup, url):
+    favicon = soup.find('link', {'rel': 'icon'})
+    favicon_link = favicon.attrs['href']
+    abs_favicon_link = urllib.parse.urljoin(url, favicon_link)
+    filename = favicon_link.split('/')[-1]
+    favicon.attrs['href'] = filename
+    urllib.request.urlretrieve(abs_favicon_link, filename)
 
 
 def make_dir():
@@ -49,11 +48,16 @@ def make_dir():
 
 
 def main():
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+    urllib.request.install_opener(opener)
     make_dir()
     url = 'https://v4-alpha.getbootstrap.com/examples/jumbotron/'
-    html = fetch_html(url)
-    links = get_links_from_html(html, url)
-    retrieve_files(links)
+    webpage = fetch_html(url)
+    soup = BeautifulSoup(webpage, 'lxml')
+    retrieve_js_files(soup, url)
+    retrieve_css_files(soup, url)
+    retrieve_favicion(soup, url)
 
 
 if __name__ == '__main__':
